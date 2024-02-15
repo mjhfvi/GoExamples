aee747fbbf7cbf4030a9f653f06fca13e213c0a5
mjhfvi <mjhfvi@gmail.com>
2024-02-15 19:19:36 +0200

    updates

--- gobyexample/go.mod
+++ gobyexample/go.mod
@@ -1,3 +1,9 @@
 module gobyexample
 
 go 1.22
+
+require (
+golang.org/x/exp v0.0.0-20240205201215-2c58cdc269a3 // indirect
+
+)
+
---
+++ gobyexample/user-input.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i string
+
+	fmt.Print("Type Your Input: ")
+	fmt.Scan(&i)
+	fmt.Println("Your Input Is:", i)
+}
